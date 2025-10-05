c1f65553b01ee8d40fa1dc5c7ab678f22f9a0537
amirAghayari <amiraghayari2119@gmail.com>
2025-10-05 12:26:39 +0330

    Add pricing , product pages

--- world-wise-client/src/App.jsx
+++ world-wise-client/src/App.jsx
@@ -3,7 +3,8 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import SpinnerFullPage from "./components/SpinnerFullPage.jsx";
 
 const Homepage = lazy(() => import("./pages/Homepage"));
-
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
 
 function App() {
 
@@ -12,6 +13,8 @@ function App() {
         <Suspense fallback={<SpinnerFullPage />}>
         <Routes>
             <Route index element={<Homepage />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/pricing" element={< Pricing/>} />
         </Routes>
         </Suspense>
     </BrowserRouter>
